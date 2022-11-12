72d2183ea058452f9b471d20b227fc4b4e18a2cf
TheSergeyPixel <thesergeypixel@gmail.com>
2022-11-12 13:35:00 +0300

    fix src

--- src.py
+++ src.py
@@ -7,6 +7,6 @@ def admix(in_file, model):
     res = StringIO(subprocess.check_output(f"admix -f {in_file} -m {model}", shell=True, text=True))
     df = pd.read_csv(res, skiprows=5)
     ser = df.iloc[:, -1].str.split(": ", expand=True)[1]
-    admix_scores = ",".join(a.strip('%') for a in ser)
+    admix_scores = ",".join([a.strip('%') for a in ser])
     return admix_scores
 
